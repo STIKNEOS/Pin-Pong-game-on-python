59765f0142d1ab656afc795121e0bd65e4cc9893
STIKNEOS <81769039+STIKNEOS@users.noreply.github.com>
2021-05-29 20:15:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,31 @@ WIN_X, WIN_Y = 600, 500
 FPS = 40
 GREEN = (50, 150, 50)
 BLUE = (50, 50, 150)
-@@ -35,12 +35,38 @@ def update(self):
+RED = (150, 50, 50)
+class GameSprite(pg.sprite.Sprite):
+    def __init__(self, color, x, y, speed, wight, height):
+        super().__init__()
+        self.image = pg.Surface((wight, height)) #вместе 55,55 - параметры
+        self.image.fill(color=color)
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+ 
+    def reset(self, window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player1(GameSprite):
+   def update(self):
+       keys = pg.key.get_pressed()
+       if keys[pg.K_UP] and self.rect.y > 0:
+           self.rect.y -= self.speed
+       if keys[pg.K_DOWN] and self.rect.y < WIN_Y - self.rect.height:
+           self.rect.y += self.speed
+class Player2(GameSprite):
+   def update(self):
+       keys = pg.key.get_pressed()
+       if keys[pg.K_w] and self.rect.y > 0:
+           self.rect.y -= self.speed
        if keys[pg.K_s] and self.rect.y < WIN_Y - self.rect.height:
            self.rect.y += self.speed
 
@@ -45,7 +69,12 @@ ball = Ball('ball.png', 200, 200, 4, 40, 40)
 
 clock = pg.time.Clock()
 run = True
-@@ -53,5 +79,9 @@ def update(self):
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+    win.fill(GREEN)
+    racket1.update()
     racket2.update()
     racket1.reset(win)
     racket2.reset(win)
