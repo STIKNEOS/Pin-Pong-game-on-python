e86eca9c0b949e09fe216972be2f5b21004a4991
STIKNEOS <81769039+STIKNEOS@users.noreply.github.com>
2021-05-29 19:47:14 +0300

    Create main.py
    Ja ja

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+WIN_X,WIN_Y = 800,500
+FPS = 40
+WAVE = (0,150,150)
+
+win = pg.display.set_mode((WIN_X,WIN_Y))
+pg.display.set_caption("Pin-Pong")
+clock = pg.time.Clock()
+run = True
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+    win.fill(WAVE)
+    pg.display.update()
+    clock.tick(FPS)
